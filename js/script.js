81cd9ad7e1ea8e3254bfea19689c00b925115873
Darima Motta <darima_mtp@mail.ru>
2021-02-24 23:05:04 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -85,4 +85,23 @@ return {
 
     }
     setClock('.timer',deadline);
+
+    //Modal
+    const modalTrigger=document.querySelectorAll('[data-modal]'),
+          modal= document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.addEventListener('click',() => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow ='hidden';
+    });
+
+        
+    modalCloseBtn.addEventListener('click',() => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow ='';
+    });
+
 });
