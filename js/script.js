b95653a518b4337a7cb6b7fcb74d920bc3f30d27
Darima Motta <darima_mtp@mail.ru>
2021-02-25 00:46:44 +0300

    modification of modal

--- js/script.js
+++ js/script.js
@@ -93,15 +93,19 @@ return {
           modalCloseBtn = document.querySelector('[data-close]');
 
           modalTrigger.forEach(btn => {
-           btn.addEventListener('click',function() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        //modal.classList.toggle('show');
-        document.body.style.overflow ='hidden';
-    });    
+           btn.addEventListener('click',openModal);
+        
+    
           });
    
+         function openModal(){
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                //modal.classList.toggle('show');
+                document.body.style.overflow ='hidden';
+                clearInterval(modalTimerId);
 
+         }
         function closeModal(){    
         modal.classList.add('hide');
         modal.classList.remove('show');
@@ -126,4 +130,15 @@ document.addEventListener('keydown',(e)=>{
         closeModal();
     }
 });
+const modalTimerId=setTimeout(openModal, 5000);
+function showModalByScroll(){
+    if (window.pageYOffset+document.documentElement.clientHeight>=document.documentElement.scrollHeight){
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+
+}
+window.addEventListener('scroll', showModalByScroll);
+    
+
 });
